f4b743749abfeaea54d9b35b6b6e6bc7b66247b6
Bikash Jaiswal <bjjaiswal@gmail.com>
2020-04-21 21:09:47 -0700

    multi RSS render

--- Headlines/headlines.py
+++ Headlines/headlines.py
@@ -3,26 +3,34 @@ import feedparser as fp
 from flask import Flask
 app = Flask(__name__)
 
-bbcFeed = 'http://feeds.bbci.co.uk/news/rss.xml'
-
+RssFeed = {'bbc':'http://feeds.bbci.co.uk/news/rss.xml',
+            'cnn': 'http://rss.cnn.com/rss/edition.rss',
+            'fox': 'http://feeds.foxnews.com/foxnews/latest',
+            'iol': 'http://www.iol.co.za/cmlink/1.640'}
+#for root
 @app.route('/')
-def get_news():
-    feed = fp.parse(bbcFeed)
-    firstArticle = feed.entries[3]
+#for bbc location
+@app.route('/bbc')
+def bbc():
+    return get_news('bbc')
+
+@app.route('/cnn')
+def cnn():
+    return get_news('cnn')
+
+def get_news(feedname):
+    feed = fp.parse(RssFeed[feedname])
+    firstArticle = feed.entries[0]
     return f"""
-                 <html>
+           <html>
                     <body>
                         <h1> BBC Headlines </h1>
                         <b>{firstArticle.title}</b> 
                         <i>{firstArticle.published}<i><br/>
                         <p>{firstArticle.summary}</p><br/>     
                     </body>
-                </html>
-                """
-    
-        
-
-   
+            </html>
+             """      
    
 if __name__ == "__main__":
     app.run(port=5000, debug = True)    
